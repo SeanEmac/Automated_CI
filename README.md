0a6086276566701c22cea4bf6fdd7e2644939b8f
seanemac <S.mccann11@nuigalway.ie>
2019-11-13 16:11:53 +0000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# SWEngineering Assignment 2
---
+++ src/main/java/nuigalway/App.java
@@ -0,0 +1,13 @@
+package nuigalway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
