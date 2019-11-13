598cd41ea15948b7a2cb7806adf21e5b26df60ca
seanemac <S.mccann11@nuigalway.ie>
2019-11-13 18:45:19 +0000

    Student class and tests, turned off emails

--- src/main/java/nuigalway/Student.java
+++ src/main/java/nuigalway/Student.java
@@ -1,9 +1,67 @@
 package nuigalway;
 
+import org.joda.time.LocalDate;
+import org.joda.time.Years;
+
 public class Student 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private String name;
+    private int age;
+    private LocalDate dob;
+    private int id;
+    private String username;
+    // private Course course
+    // private List<Module> modules
+
+    public Student(String name, LocalDate dob, int id){
+        this.name = name;
+        this.dob = dob;
+        this.id = id;
+
+        setAge();
+        setUsername();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        setUsername();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setAge() {
+        this.age = Years.yearsBetween(dob, new LocalDate()).getYears();
+        setUsername();
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+        setAge();
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setUsername() {
+        this.username = name + age;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
--- src/test/java/nuigalway/StudentTest.java
+++ src/test/java/nuigalway/StudentTest.java
@@ -1,14 +1,42 @@
 package nuigalway;
 
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+import org.joda.time.LocalDate;
+import org.junit.Test;
 
 public class StudentTest
 {
+    private String name = "bob";
+    private LocalDate dob = new LocalDate(1997, 06, 27);
+    private int id = 16343111;
+    private int age = 22;
+    private String username = "bob22";
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldCreateStudent()
     {
-        assertTrue( true );
+        Student stu = new Student(name, dob, id);
+        assertEquals(stu.getName(), name);
+        assertEquals(stu.getDob(), dob);
+        assertEquals(stu.getId(), id);
+        assertEquals(stu.getAge(), age);
+        assertEquals(stu.getUsername(), username);
+    }
+
+    @Test
+    public void shouldUpdateStudent() {
+        LocalDate newDob = new LocalDate(2001, 06, 27);
+        String newName = "barry";
+        int newAge = 18;
+        String newUsername = "barry18";
+
+        Student stu = new Student(name, dob, id);
+        stu.setDob(newDob);
+        assertEquals(stu.getDob(), newDob);
+        assertEquals(stu.getAge(), newAge);
+        stu.setName(newName);
+        assertEquals(stu.getName(), newName);
+        assertEquals(stu.getUsername(), newUsername);
     }
 }
